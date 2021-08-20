b3de3f575069e10485384dd00a820d88578dd157
Ovidiu Stoica <ostoica@c.fitbit.com>
2021-08-20 10:45:42 +0300

    Initial commit

---
+++ test/brave_and_true_code/core_test.clj
@@ -0,0 +1,7 @@
+(ns brave-and-true-code.core-test
+  (:require [clojure.test :refer :all]
+            [brave-and-true-code.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
